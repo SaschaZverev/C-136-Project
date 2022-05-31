e90225553618fb81fe7d27bdb70379251adc5c9e
SaschaZverev <98932516+SaschaZverev@users.noreply.github.com>
2022-05-31 17:10:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+s = "";
+input_text = "";
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+  image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+  document.getElementById("status").innerHTML = "Status: Detecting Objects";
+  input_text = document.getElementById("input_id").value;
+}
+
+function modelLoaded(){
+  console.log("Model Loaded !");
+  s = true;
+}
